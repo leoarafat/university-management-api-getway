838f75c6587eb0505f176276e4615ef5e8e8a13d
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2023-09-18 19:18:23 +0800

    [DONE] user creation with api gateway

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,14 +1,30 @@
 import express from 'express';
 import { academicSemesterRoutes } from '../modules/academicSemester/academicSemester.routes';
 import { academicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes';
+import { academicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.routes';
+import { buildingRoutes } from '../modules/building/building.routes';
+import { roomRoutes } from '../modules/room/room.routes';
+import { courseRoutes } from '../modules/course/course.routes';
+import { semesterRegistrationRoutes } from '../modules/semesterRegistration/semesterRegistration.routes';
+import { offeredCourseRoutes } from '../modules/offeredCourse/offeredCourse.routes';
+import { offeredCourseSectionRoutes } from '../modules/offeredCourseSection/offeredCourseSection.routes';
+import { offeredCourseClassScheduleRoutes } from '../modules/offeredCourseClassSchedule/offeredCourseClassSchedule.routes';
+import { studentEnrolledCourseRoutes } from '../modules/studentEnrolledCourse/studentEnrolledCourse.routes';
+import { studentEnrolledCourseMarkRoutes } from '../modules/studentEnrolledCourseMark/studentEnrolledCourseMark.routes';
+import { studentSemesterPaymentRoutes } from '../modules/studentSemesterPayment/studentSemesterPayment.routes';
+import { userRoutes } from '../modules/user/user.routes';
 
 const router = express.Router();
 
 const moduleRoutes = [
-  // {
-  //     path: '/academic-faculties',
-  //     routes: academicFacultyRoutes
-  // },
+  {
+    path: '/users',
+    routes: userRoutes
+  },
+  {
+    path: '/academic-faculties',
+    routes: academicFacultyRoutes
+  },
   {
     path: '/academic-departments',
     routes: academicDepartmentRoutes
@@ -16,47 +32,47 @@ const moduleRoutes = [
   {
     path: '/academic-semesters',
     routes: academicSemesterRoutes
+  },
+  {
+    path: '/buildings',
+    routes: buildingRoutes
+  },
+  {
+    path: '/rooms',
+    routes: roomRoutes
+  },
+  {
+    path: '/courses',
+    routes: courseRoutes
+  },
+  {
+    path: '/semester-registrations',
+    routes: semesterRegistrationRoutes
+  },
+  {
+    path: '/offered-courses',
+    routes: offeredCourseRoutes
+  },
+  {
+    path: '/offered-course-sections',
+    routes: offeredCourseSectionRoutes
+  },
+  {
+    path: '/offered-course-class-schedules',
+    routes: offeredCourseClassScheduleRoutes
+  },
+  {
+    path: '/student-enrolled-courses',
+    routes: studentEnrolledCourseRoutes
+  },
+  {
+    path: '/student-enrolled-course-marks',
+    routes: studentEnrolledCourseMarkRoutes
+  },
+  {
+    path: '/student-semester-payments',
+    routes: studentSemesterPaymentRoutes
   }
-  // {
-  //     path: '/buildings',
-  //     routes: buildingRoutes
-  // },
-  // {
-  //     path: '/rooms',
-  //     routes: roomRoutes
-  // },
-  // {
-  //     path: '/courses',
-  //     routes: courseRoutes
-  // },
-  // {
-  //     path: '/semester-registrations',
-  //     routes: semesterRegistrationRoutes
-  // },
-  // {
-  //     path: '/offered-courses',
-  //     routes: offeredCourseRoutes
-  // },
-  // {
-  //     path: '/offered-course-sections',
-  //     routes: offeredCourseSectionRoutes
-  // },
-  // {
-  //     path: '/offered-course-class-schedules',
-  //     routes: offeredCourseClassScheduleRoutes
-  // },
-  // {
-  //     path: '/student-enrolled-courses',
-  //     routes: studentEnrolledCourseRoutes
-  // },
-  // {
-  //     path: '/student-enrolled-course-marks',
-  //     routes: studentEnrolledCourseMarkRoutes
-  // },
-  // {
-  //     path: '/student-semester-payments',
-  //     routes: studentSemesterPaymentRoutes
-  // }
 ];
 
 moduleRoutes.forEach((route) => {
